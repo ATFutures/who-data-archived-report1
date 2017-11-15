b51e8e458fde66062ef6fe7199caf4b6d86cb852
mpadge <mark.padgham@email.com>
2017-11-15 10:04:46 +0000

    add R pkg structure

---
+++ R/crop-tif.R
@@ -0,0 +1,40 @@
+#' crop_worldpop_tif
+#'
+#' \code{.zip} files downloaded from worldpop are national-scale. This function
+#' crops them to the designated cities, and converts them to much smaller geotif
+#' files.
+#'
+#' @param city Name of city (one of "accra", "kathmandu", or "bristol")
+#' @param expand Relative factor by which to expand cropping limits beyond
+#' rectangular bounding box of city
+#' @return Nothing Files are written to the corresponding directories
+#' @export
+crop_tif <- function (city = "accra", expand = 0.1)
+{
+    bb <- osmdata::getbb (city)
+    bb_exp <- t (apply (bb, 1, function (i)
+                        i + c (-expand, expand) * diff (i)))
+    wd <- file.path (here::here (), city, "worldpop")
+    files <- list.files (wd)
+    files <- file.path (wd, files [grep ("\\.zip", files)])
+
+    pb <- txtProgressBar (style = 3)
+    count <- 1
+    n <- length (files)
+    for (f in files)
+    {
+        zfiles <- unzip (f, list = TRUE)$Name
+        f1 <- zfiles [grep ("tif$", zfiles, ignore.case = TRUE)]
+        unzip (f, files = f1)
+        r <- raster::raster (f1) %>% raster::crop (bb_exp)
+        f0 <- paste0 (tools::file_path_sans_ext (f1), ".zip")
+        file.remove (file.path (wd, f0))
+        # faster convert file names to lower case, so
+        f1 <- paste0 (tools::file_path_sans_ext (f1), ".tif")
+        f1 <- file.path (wd, f1)
+        raster::writeRaster (r, f1)
+        setTxtProgressBar (pb, count / n)
+        count <- count + 1
+    }
+    close (pb)
+}
---
+++ R/whodata-package.R
@@ -0,0 +1,12 @@
+#' whodata
+#'
+#' @name whodata
+#' @docType package
+#' @author Mark Padgham, Robin Lovelace
+#' @importFrom here here
+#' @importFrom magrittr %>% extract2
+#' @importFrom osmdata add_osm_feature getbb opq osmdata_sf trim_osmdata
+#' @importFrom raster crop raster writeRaster
+#' @importFrom utils packageVersion setTxtProgressBar
+#' @importFrom utils txtProgressBar unzip
+NULL
